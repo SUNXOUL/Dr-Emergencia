7e9bcab63d97969aae3ba73116306223539b97d2
Kevin Duran Bruno <123a2017kdb@gmail.com>
2023-02-17 10:18:34 -0400

    rebuild db

---
+++ Migrations/20230217141635_Inicial.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DrEmergencias.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DetallesEmergencias",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PacienteID = table.Column<int>(type: "INTEGER", nullable: false),
+                    OrdenID = table.Column<int>(type: "INTEGER", nullable: false),
+                    PrimerNombre = table.Column<string>(type: "TEXT", nullable: false),
+                    SegundoNombre = table.Column<string>(type: "TEXT", nullable: false),
+                    PrimerApellido = table.Column<string>(type: "TEXT", nullable: false),
+                    SegundoApellido = table.Column<string>(type: "TEXT", nullable: false),
+                    Sexo = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cedula = table.Column<string>(type: "TEXT", nullable: false),
+                    CedulaTutor = table.Column<string>(type: "TEXT", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Medico = table.Column<string>(type: "TEXT", nullable: false),
+                    Visible = table.Column<bool>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetallesEmergencias", x => x.DetalleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetallesEmergenciasA",
+                columns: table => new
+                {
+                    DetalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PacienteID = table.Column<int>(type: "INTEGER", nullable: false),
+                    OrdenID = table.Column<int>(type: "INTEGER", nullable: false),
+                    PrimerNombre = table.Column<string>(type: "TEXT", nullable: false),
+                    SegundoNombre = table.Column<string>(type: "TEXT", nullable: false),
+                    PrimerApellido = table.Column<string>(type: "TEXT", nullable: false),
+                    SegundoApellido = table.Column<string>(type: "TEXT", nullable: false),
+                    Sexo = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cedula = table.Column<string>(type: "TEXT", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Medico = table.Column<string>(type: "TEXT", nullable: false),
+                    Ars = table.Column<int>(type: "INTEGER", nullable: false),
+                    NSS = table.Column<int>(type: "INTEGER", nullable: false),
+                    Visible = table.Column<bool>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetallesEmergenciasA", x => x.DetalleId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Emergencia",
+                columns: table => new
+                {
+                    OrdenID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EmergenciaID = table.Column<string>(type: "TEXT", nullable: true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    MConsulta = table.Column<string>(name: "M_Consulta", type: "TEXT", nullable: true),
+                    Antecedentes = table.Column<string>(type: "TEXT", nullable: true),
+                    AtencionesPrevias = table.Column<bool>(type: "INTEGER", nullable: true),
+                    Donde = table.Column<string>(type: "TEXT", nullable: true),
+                    MedicoBase = table.Column<string>(type: "TEXT", nullable: true),
+                    Peso = table.Column<int>(type: "INTEGER", nullable: true),
+                    TA = table.Column<int>(type: "INTEGER", nullable: true),
+                    FC = table.Column<int>(type: "INTEGER", nullable: true),
+                    FR = table.Column<int>(type: "INTEGER", nullable: true),
+                    Temperatura = table.Column<int>(type: "INTEGER", nullable: true),
+                    Diagnostico = table.Column<string>(type: "TEXT", nullable: true),
+                    Hemogramas = table.Column<int>(type: "INTEGER", nullable: true),
+                    ExOrina = table.Column<int>(name: "Ex_Orina", type: "INTEGER", nullable: true),
+                    Glisemia = table.Column<int>(type: "INTEGER", nullable: true),
+                    Coprologico = table.Column<int>(type: "INTEGER", nullable: true),
+                    Imagenes = table.Column<int>(type: "INTEGER", nullable: true),
+                    Especifique = table.Column<string>(type: "TEXT", nullable: true),
+                    TyM = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emergencia", x => x.OrdenID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmergenciaA",
+                columns: table => new
+                {
+                    OrdenID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EmergenciaID = table.Column<string>(type: "TEXT", nullable: true),
+                    ARS = table.Column<int>(type: "INTEGER", nullable: true),
+                    NSS = table.Column<int>(type: "INTEGER", nullable: true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Hora = table.Column<int>(type: "INTEGER", nullable: true),
+                    MConsulta = table.Column<string>(name: "M_Consulta", type: "TEXT", nullable: true),
+                    Antecedentes = table.Column<string>(type: "TEXT", nullable: true),
+                    AtencionesPrevias = table.Column<bool>(type: "INTEGER", nullable: true),
+                    Donde = table.Column<string>(type: "TEXT", nullable: true),
+                    MedicoBase = table.Column<string>(type: "TEXT", nullable: true),
+                    Peso = table.Column<int>(type: "INTEGER", nullable: true),
+                    TA = table.Column<int>(type: "INTEGER", nullable: true),
+                    FC = table.Column<int>(type: "INTEGER", nullable: true),
+                    FR = table.Column<int>(type: "INTEGER", nullable: true),
+                    Temperatura = table.Column<int>(type: "INTEGER", nullable: true),
+                    Cabeza = table.Column<string>(type: "TEXT", nullable: true),
+                    Cuello = table.Column<string>(type: "TEXT", nullable: true),
+                    Torax = table.Column<string>(type: "TEXT", nullable: true),
+                    Pulmones = table.Column<string>(type: "TEXT", nullable: true),
+                    Corazon = table.Column<string>(type: "TEXT", nullable: true),
+                    Abdomen = table.Column<string>(type: "TEXT", nullable: true),
+                    EInferiores = table.Column<string>(name: "E_Inferiores", type: "TEXT", nullable: true),
+                    ESuperiores = table.Column<string>(name: "E_Superiores", type: "TEXT", nullable: true),
+                    Diagnostico = table.Column<string>(type: "TEXT", nullable: true),
+                    Hemogramas = table.Column<int>(type: "INTEGER", nullable: true),
+                    ExOrina = table.Column<int>(name: "Ex_Orina", type: "INTEGER", nullable: true),
+                    Glisemia = table.Column<int>(type: "INTEGER", nullable: true),
+                    Coprologico = table.Column<int>(type: "INTEGER", nullable: true),
+                    Imagenes = table.Column<int>(type: "INTEGER", nullable: true),
+                    Especifique = table.Column<string>(type: "TEXT", nullable: true),
+                    TyM = table.Column<string>(type: "TEXT", nullable: true),
+                    Visible = table.Column<bool>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmergenciaA", x => x.OrdenID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Paciente",
+                columns: table => new
+                {
+                    PacienteID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PrimerNombre = table.Column<string>(type: "TEXT", nullable: true),
+                    SegundoNombre = table.Column<string>(type: "TEXT", nullable: true),
+                    PrimerApellido = table.Column<string>(type: "TEXT", nullable: true),
+                    SegundoApellido = table.Column<string>(type: "TEXT", nullable: true),
+                    Sexo = table.Column<int>(type: "INTEGER", nullable: true),
+                    Cedula = table.Column<string>(type: "TEXT", nullable: true),
+                    Ocupacion = table.Column<string>(type: "TEXT", nullable: true),
+                    Tutor = table.Column<string>(type: "TEXT", nullable: true),
+                    CedulaTutor = table.Column<string>(type: "TEXT", nullable: true),
+                    Alergia = table.Column<string>(type: "TEXT", nullable: true),
+                    Telefono = table.Column<string>(type: "TEXT", nullable: true),
+                    FechadeNacimiento = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Edad = table.Column<int>(type: "INTEGER", nullable: true),
+                    Direccion = table.Column<string>(type: "TEXT", nullable: true),
+                    Provincia = table.Column<int>(type: "INTEGER", nullable: true),
+                    Municipio = table.Column<string>(type: "TEXT", nullable: true),
+                    Visible = table.Column<bool>(type: "INTEGER", nullable: true),
+                    EmergenciaAOrdenID = table.Column<int>(type: "INTEGER", nullable: true),
+                    EmergenciaOrdenID = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Paciente", x => x.PacienteID);
+                    table.ForeignKey(
+                        name: "FK_Paciente_EmergenciaA_EmergenciaAOrdenID",
+                        column: x => x.EmergenciaAOrdenID,
+                        principalTable: "EmergenciaA",
+                        principalColumn: "OrdenID");
+                    table.ForeignKey(
+                        name: "FK_Paciente_Emergencia_EmergenciaOrdenID",
+                        column: x => x.EmergenciaOrdenID,
+                        principalTable: "Emergencia",
+                        principalColumn: "OrdenID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Paciente_EmergenciaAOrdenID",
+                table: "Paciente",
+                column: "EmergenciaAOrdenID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Paciente_EmergenciaOrdenID",
+                table: "Paciente",
+                column: "EmergenciaOrdenID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetallesEmergencias");
+
+            migrationBuilder.DropTable(
+                name: "DetallesEmergenciasA");
+
+            migrationBuilder.DropTable(
+                name: "Paciente");
+
+            migrationBuilder.DropTable(
+                name: "EmergenciaA");
+
+            migrationBuilder.DropTable(
+                name: "Emergencia");
+        }
+    }
+}
