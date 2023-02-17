24877d8d542c357c7d6239cd97361ad737d7a672
Kevin Duran Bruno <123a2017kdb@gmail.com>
2023-02-17 13:12:57 -0400

    Con error en lista de pacientes

--- Models/Detalles/DetalleEmergencia.cs
+++ Models/Detalles/DetalleEmergencia.cs
@@ -11,17 +11,17 @@ namespace DrEmergencias
 
         public  int OrdenID{get;set;}
 
-        public string PrimerNombre { get; set; }
-        public string SegundoNombre { get; set; }
-        public string PrimerApellido { get; set; }
+        public string? PrimerNombre { get; set; }
+        public string? SegundoNombre { get; set; }
+        public string? PrimerApellido { get; set; }
 
-        public string SegundoApellido { get; set; }
-        public Sexo Sexo { get; set; }
+        public string? SegundoApellido { get; set; }
+        public Sexo? Sexo { get; set; }
 
-        public string Cedula { get; set; }
-        public string CedulaTutor { get; set; }
+        public string? Cedula { get; set; }
+        public string? CedulaTutor { get; set; }
         public DateTime? Fecha { get; set; }
-        public string Medico { get;set; }
+        public string? Medico { get;set; }
         
         
         public bool? Visible {get; set;}
